c0da5d1d70879c74d4af5cb3fd125928cd6a1643
tbaharvand <tarabaharvand97@gmail.com>
2025-02-14 22:51:56 +0330

    add kapt and lifecycle-viewmodel-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,25 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //alias(libs.plugins.kotlin.kapt)
+//    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
+//    alias(libs.plugins.dagger.hilt.android)
+    alias (libs.plugins.kotlin.kapt)
 }
 
 android {
     namespace = "com.example.cleanarchitecturenoteapp"
     compileSdk = 34
+
+    //step1
+    buildFeatures {
+        compose=true
+    }
+//    buildscript{
+//        ext {
+//            composeVersion = "1.4.3"
+//        }
+//    }
 
     defaultConfig {
         applicationId = "com.example.cleanarchitecturenoteapp"
@@ -43,7 +57,17 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation("androidx.compose:compose-bom:2025.02.00")
+
+   // implementation (libs.hilt.android)
+
+
 }
--- app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/model/Notes.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/model/Notes.kt
@@ -1,4 +1,10 @@
 package com.example.cleanarchitecturenoteapp.feature_note.domain.model
 
 //need to add room and other dependencies
-data class Notes()
+
+data class Notes(
+    val title: String,
+    val content: String,
+    val timeStamp: Long,
+    val color: Int
+)
